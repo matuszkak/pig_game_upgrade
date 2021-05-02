043a6598ed20ada6ee8b2b89037125a13e4be8a1
Kristof Matuszka <matuszkak@gmail.com>
2021-05-02 22:32:40 +0200

    double dice verzió kész

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 
-//kell két új változó: lastRolls = utolsó dobásokat rögzíti, winningscore: nyerési pontszám paraméterehzető
-let scores, roundScore, activePlayer, lastRolls, winningscore;
+let scores, roundScore, activePlayer, winningscore;
 
 winningscore = 100;
 
 // UI inicializálás új kör kezdéskor
+
 function init() {
-  // pontszámok kezdő értékei
+
   scores = [0, 0];
-  lastRolls = [0, 0];
   roundScore = 0;
   activePlayer = 0;
-
-
-  //nyerési pontszámot update-eljük ha volt változtatás, ha nem akkor meg beírjuk a defaultot
 
   // ha töltve van a mező...
   if (document.querySelector('.final-score').value !== "") {
@@ -35,9 +31,11 @@ function init() {
   document.querySelector('#current-0').textContent = 0;
   document.querySelector('#current-1').textContent = 0;
 
-  // a játék kezdetekor a kockák eltűntetése, gombok UI-ra - induló állapot
+  // a játék kezdetekor a két kockát eltüntetjük:
   document.querySelector('#dice-1').style.display = 'none';
   document.querySelector('#dice-2').style.display = 'none';
+
+  // a gombokat megjelenítjük
   document.querySelector('.btn-roll').style.display = 'block';
   document.querySelector('.btn-hold').style.display = 'block';
 
@@ -51,9 +49,36 @@ function init() {
   document.querySelector('.player-0-panel').classList.add('active');
 }
 
-// játékos váltás hold, 1-es vagy dupla 6-os esetén
+init();
+
+// új játék kezdete...
+document.querySelector('.btn-new').addEventListener('click', init);
+
+// ha a roll dice gombra kattint a user...
+document.querySelector('.btn-roll').addEventListener('click', function () {
+
+  let dice1 = Math.floor(Math.random() * 6) + 1;
+  let dice2 = Math.floor(Math.random() * 6) + 1;
+
+  let diceDOM1 = document.querySelector('#dice-1');
+  diceDOM1.style.display = 'block';
+  diceDOM1.setAttribute('src', 'dice-' + dice1 + '.png');
+  let diceDOM2 = document.querySelector('#dice-2');
+  diceDOM2.style.display = 'block';
+  diceDOM2.setAttribute('src', 'dice-' + dice2 + '.png');
+
+  // Ha a ha játékos legalább egy 1-est dob, a roundScore értékét elveszti és a másik jön
+
+  if (dice1 !== 1 && dice2 !== 1) {
+    roundScore = roundScore + dice1 + dice2;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    nextPlayer();
+  }
+});
+
 function nextPlayer() {
-  // roundScore értéket nullázzuk a UI-on
+  // roundScore értéket nullázzuk a UI-on is:
   document.querySelector('#current-' + activePlayer).textContent = 0;
   // a következő játékos jön
   if (activePlayer === 0) {
@@ -61,85 +86,34 @@ function nextPlayer() {
   } else {
     activePlayer = 0;
   }
-  //nullázzuk a current score-t és az előző dobás értékét
   roundScore = 0;
-  lastRolls[activePlayer] = 0;
   // toggle: ha rajta volt a class akkor leveszi, ha nem volt rajta
+  // akkor rárakja...
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
 }
 
-// Innen indul a játék logika...
-
-// indító képernyő inicializálása
-init();
-
-// új játék gomb lenyomásra inító képernyő...
-document.querySelector('.btn-new').addEventListener('click', init);
-
-// ha a roll dice gombra kattint a user...
-document.querySelector('.btn-roll').addEventListener('click', function () {
-  let dice = Math.floor(Math.random() * 6) + 1;
-
-  // ha nem 6-ost dob...
-  if (dice !== 6) {
-    // az előző dobás rögzített értéke lastRolls[activePlayer] nem számít + töröljük a második kockát ha aktív volt
-    let diceDOM = document.querySelector('#dice-2');
-    diceDOM.style.display = 'none';
-    lastRolls[activePlayer] = 0;
-
-    // megjelenítjük a dobást az első kockán
-    diceDOM = document.querySelector('#dice-1');
-    diceDOM.setAttribute('src', 'dice-' + dice + '.png')
-    diceDOM.style.display = 'block';
-
-    // ha 1-est dob, a roundScore értékét elveszti és a másik jön
-    if (dice !== 1) {
-      roundScore = roundScore + dice;
-      document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-      nextPlayer();
-    }
-    // ha 6-ost dob
-  } else {
-    // ...az első 6-osnál még semmi extra csak rögzítjük a dobást értékét a lastRolls változóban
-    lastRolls[activePlayer] = lastRolls[activePlayer] + dice;
-    if (lastRolls[activePlayer] !== 12) {
-      roundScore = roundScore + dice;
-      document.querySelector('#current-' + activePlayer).textContent = roundScore;
-      let diceDOM = document.querySelector('#dice-1');
-      diceDOM.style.display = 'block';
-      diceDOM.setAttribute('src', 'dice-' + dice + '.png');
-      // második 6-ost jelző kockát levesszük ha esetleg ott volt a megelőző dobás után
-      diceDOM = document.querySelector('#dice-2');
-      diceDOM.style.display = 'none';
-
-    } else {
-      // a második 6-osnál a második kocka is megjelenik felső alatt és a játékos bukja a hold-olt pontjait + csere van
-      scores[activePlayer] = 0;
-      document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-      let diceDOM2 = document.querySelector('#dice-2');
-      diceDOM2.setAttribute('src', 'dice-' + dice + '.png')
-      diceDOM2.style.display = 'block';
-      nextPlayer();
-    }
-  }
-});
 
 // ha a hold gombra rányom a játékos
 document.querySelector('.btn-hold').addEventListener('click', function () {
   // a játékos megszerzi a kör alatt szerzett pontjait
+  // az előző érték plusz a mostani...
   scores[activePlayer] = scores[activePlayer] + roundScore;
+  // update the UI
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-  // van-e nyertes?
+
+  // ellenőrizzük hogy van e nyertes:
   if (scores[activePlayer] >= winningscore) {
-    // ha igen akkor vége a játéknak
+    // játék vége
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('#dice-1').style.display = 'none';
+    document.querySelector('#dice-2').style.display = 'none';
     document.querySelector('.btn-roll').style.display = 'none';
     document.querySelector('.btn-hold').style.display = 'none';
+
     // ha nincs nyertes, akkor a következő játékos jön
   } else {
     nextPlayer();
